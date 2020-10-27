db0e0bdc5d55171acd95524f54f25ddbd899fd71
Junrui Liu <andyliu0248@gmail.com>
2020-10-26 21:18:12 -0400

    Change benchmark case format

--- benchmark.py
+++ benchmark.py
@@ -7,7 +7,7 @@ def parseArg():
     CMD argument parsing
     :return: the parser
     """
-    parser = argparse.ArgumentParser(description='Blotto solver.')
+    parser = argparse.ArgumentParser(description='Simplex Benchmarking')
     parser.add_argument('file', metavar='fpath', type=str)
     return parser
 
@@ -17,15 +17,22 @@ if __name__ == "__main__":
     parser = lp.formula_parser()
     transformer = lp.FormulaTransformer()
     with open(args.file, 'r') as f:
+        case = []
         for line in f:
             line = line.strip()
             if len(line) == 0 or line[0] == '%':
                 continue
-            par_tree = parser.parse(line)
-            print(line)
-            print('----')
-            formula = transformer.transform(par_tree)
-            lp_prob = lp.Opti(formula)
-            lp_prob.simplex()
-            print('='*80)
+            case.append(line)
+            if len(case) == 2:
+                formula, expected = case
+                print(formula)
+                res = lp.run(formula)
+                print(res)
+                if 'UNSAT' in expected and 'UNSAT' in res or \
+                    'UNSAT' not in expected and 'UNSAT' not in res:
+                    print("Passed")
+                else:
+                    print("Failed. Expected:", expected)
+                case = []
+                print('='*80)
 
